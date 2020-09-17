61961ab4116071acae9715a7ffb398bb0fbb1a71
Olson <C067460@corp.cvs.com>
2020-09-17 13:09:42 -0400

    try again here

---
+++ TicketMonitor/resources/app/src/main/main.js
@@ -0,0 +1,17 @@
+  const fs = require('fs');
+  const path = require('path');
+  const config = require('../resources/config.json');
+  const customTitlebar = require('custom-electron-titlebar');
+
+  new customTitlebar.Titlebar({
+    backgroundColor: customTitlebar.Color.fromHex('#000')
+  });
+
+
+  document.addEventListener("keydown", function (e) {
+    if (e.which === 123) {
+      require('electron').remote.getCurrentWindow().webContents.openDevTools();
+    } else if (e.which === 116) {
+      location.reload();
+    }
+  });
