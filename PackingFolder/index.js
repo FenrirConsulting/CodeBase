b350ac7bdc6eed3001419c572c4d8c81ce932731
Olson <C067460@corp.cvs.com>
2020-09-18 17:46:42 -0400

    09/18 NIght Work

---
+++ PackingFolder/index.js
@@ -0,0 +1,50 @@
+/*
+    Written by Christopher Olson 09-16-2020
+    For CVS Health
+*/
+
+const {
+    app,
+    BrowserWindow,
+    Menu
+} = require('electron');
+
+
+const electron = require('electron');
+const os = require('os');
+const path = require('path');
+const sql = require('mssql');
+const username = os.userInfo().username;
+let mainWindow; 
+
+app.disableHardwareAcceleration();
+
+app.on('ready', () => {
+
+    const mainHTML = ('./src/main/main.html');
+
+    windowLauncher(mainHTML); 
+    function windowLauncher(fileName){
+
+        mainWindow = new BrowserWindow({
+            webPreferences: {
+                nodeIntegration: true, 
+                enableRemoteModule: true
+            },
+            autoHideMenuBar: true,
+            fullscreen: false,
+            frame: false,
+            devTools: true,
+            maximize : true
+        });
+        mainWindow.loadFile(fileName);
+        mainWindow.maximize();
+        
+    }
+})
+
+
+
+app.on('window-all-closed', function () {
+    app.quit()
+})
--- TicketMonitor/resources/app/src/main/main.js
+++ TicketMonitor/resources/app/src/main/main.js
@@ -1,12 +1,43 @@
   const fs = require('fs');
   const path = require('path');
-  const config = require('../resources/config.json');
   const customTitlebar = require('custom-electron-titlebar');
+  const sql = require('mssql')
+  var file = '../../../../localDatabase.sdf';
+  var resolvedPath = path.resolve(__dirname,file);
+ 
+  var config = {
+    server : 'localhost',
+  };
+
+
+
+  window.onload = function() {
+    
+    sql.connect(config, function (err) {
+    
+      if (err) console.log(err);
+  
+      // create Request object
+      var request = new sql.Request();
+         
+      // query to the database and get the records
+      request.query('select * from Tickets', function (err, recordset) {
+          
+          if (err) console.log(err)
+  
+          // send records as a response
+          res.send(recordset);
+          
+      });
+  });
+
+  };
+
+  
 
   new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex('#000')
   });
-
 
   document.addEventListener("keydown", function (e) {
     if (e.which === 123) {
