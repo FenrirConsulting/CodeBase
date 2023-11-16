cafd418fd0ad44c7ce68b56a1c19ce6d5062c1e2
1444437 <Christopher.Olson2@CVSHealth.com>
2023-11-16 13:05:18 -0500

    Add IAMHAuthorizationSettings 11/16/2023

---
+++ RefractorFiles/IAMHAuthorizationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeimdallCloud.Infrastructure.Shared.DataAccess
+{
+    public  class IAMHAuthorizationSettings
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ?Type { get; set; }
+
+        public string ?Rule { get; set; }
+
+        public string ?Value { get; set; }
+
+        public string ?ReportsAssignmentGroup { get; set; }
+    }
+}
