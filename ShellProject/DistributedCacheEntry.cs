690e858edc79d2275174e551f34c873033680b2b
1444437 <Christopher.Olson2@CVSHealth.com>
2024-07-18 12:06:08 -0400

    Added DB Files

---
+++ ShellProject/DistributedCacheEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVSHealth.IAM.IAPF.Tools.WebCoreUtility.Domain.Entities
+{
+    public class DistributedCacheEntry
+    {
+        [Key]
+        [StringLength(449)]
+        public string? Id { get; set; }
+        [Required]
+        public byte[]? Value { get; set; }
+        [Required]
+        public DateTimeOffset ExpiresAtTime { get; set; }
+        public long? SlidingExpirationInSeconds { get; set; }
+        public DateTimeOffset? AbsoluteExpiration { get; set; }
+    }
+}
